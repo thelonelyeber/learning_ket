a1c969bb364839f05c705a1e7b496616bc221ec9
thelonelyeber <szemlyanin1@mail.ru>
2022-04-25 09:35:37 +0300

    почти готово...

--- Gl4_Nasledovan/class_task.py
+++ Gl4_Nasledovan/class_task.py
@@ -12,9 +12,6 @@ class Automobile(Transport):
         self.mileage = mileage  # Пробег
         self.tech_inspection = tech_inspection  # Пройден ли техосмотр
 
-    def red_brand(self):
-        self.brand = str(input('Введите имя: '))
-
     def __repr__(self):
         return f'Номер: {self.number}; Мощность: {self.power}; Стоимость: {self.cost}; Марка: {self.brand}; ' \
                f'Пробег: {self.mileage}; Прохождение техосмотра: {self.tech_inspection} '
@@ -25,27 +22,20 @@ class Aircraft(Transport):
         super().__init__(power, cost, number, brand)
         self.max_flight_altitude = max_flight_altitude  # Максимальная высота полета
 
+    def __repr__(self):
+        return
 
-car_list = list()
-i = 0
 
-# auto = Automobile(520, 2500000, 7388, 'Marusya', 90, True)
-# print(auto)
-# car_list.append(auto)
-#
-# print(f'Марка машины - {car_list[0].brand}\n'
-#       f'Стоимость - {car_list[0].cost}')
-#
-# car_list[0].red_name()
-# print(car_list[0].brand)
+cars_list = list()
+airplanes_list = list()
 
 while True:
     com = input('Введите комманду: ')
-    if com == '1':
-        power = input('Введите мощность машины: ')
+    if com == '1':  # Создание машины
+        power = input('Введите мощность: ')
         cost = input('Введите стоимость: ')
-        number = input('Введите номер машины: ')
-        brand = input('Введите марку машины: ')
+        number = input('Введите номер: ')
+        brand = input('Введите марку: ')
         mileage = input('Введите пробег: ')
         tech_inspection = input('Пройден ли техосмотр (y/n): ')
         if tech_inspection == 'y':
@@ -55,12 +45,27 @@ while True:
         else:
             print('Используйте англ раскладку и введите "y" для подтверждения или "n" в ином случае!')
         auto = Automobile(power, cost, number, brand, mileage, tech_inspection)
-        car_list.append(auto)
-        i += 1
-    elif com == '2':  # Вывод всех машин
+        cars_list.append(auto)
+    elif com == '2':  # Вывод полной информации обо всех машинах и самолетах
         count = 1
-        for car in car_list:
+        print('Машины:')
+        for car in cars_list:
             print(f'{count}) {car}')
             count += 1
+        count = 1
+        print('Самолеты:')
+        for airplane in airplanes_list:
+            print(f'{count}) {airplane}')
+            count += 1
+    elif com == '3':  # Создание самолета
+        pass
+    elif com == '4':  # Проверить, прошел ли владелец самой дорогой машины техосмотр
+        pass
+    elif com == '5':  # Подсчитать налог с регистрации всех машин
+        pass
+    elif com == '6':  # Подсчитать налог с регистрации всез самолетов
+        pass
+    elif com == '7':  # Определить мощность, стоимость и марку самого дорогого самолета
+        pass
     elif com == 'exit':
         break
