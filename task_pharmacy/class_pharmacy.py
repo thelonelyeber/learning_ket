778c8cbf276e38dd328cdb1dabf9f52a19afa0ac
thelonelyeber <szemlyanin1@mail.ru>
2022-04-26 10:16:11 +0300

    готово

--- task_pharmacy/class_pharmacy.py
+++ task_pharmacy/class_pharmacy.py
@@ -2,29 +2,42 @@ class Pharmacy(object):
     """Инициализация полей"""
 
     def __init__(self, name):
-        self._name = name
-
-    """Выводим название аптеки"""
-
-    def print_name(self):
-        pass
+        self.data = {
+            'name': name,
+            'preparats': {
+                'name': [],
+                'cost': []
+            }
+        }
 
     """Выводим стоимость всех лекарств"""
 
-    def assortment(self):
-        pass
+    def total_cost(self):
+        cost = 0
+        for phar in self.data['preparats']['cost']:
+            cost += phar
+        print(cost)
 
     """Выводим самую высокую цену за лекартсво"""
 
     def max_price(self):
-        pass
+        cost = max(self.data['preparats']['cost'])
+        print(f'Максимальная цена на лекарство в данной аптеке: {cost} р.')
 
     """Выводим полную информацию об аптеке"""
 
     def full_info(self):
-        pass
+        i = 1
+        c = 0
+        print(f'Название аптеки - {self.data["name"]}')
+        for name in self.data['preparats']['name']:
+            print(f'    {i}) {name} - {self.data["preparats"]["cost"][c]}')
+            i += 1
+            c += 1
 
     """Добавляем лекартсва от пользователя"""
 
-    def add_pill(self):
-        pass
+    def add_pill(self, prep_name, prep_cost):
+        print('Добавление препарата от пользователя')
+        self.data['preparats']['name'].append(prep_name)
+        self.data['preparats']['cost'].append(prep_cost)
--- task_pharmacy/main.py
+++ task_pharmacy/main.py
@@ -1,40 +1,43 @@
 from class_pharmacy import Pharmacy
 
-# flag = True
-# while flag:
-#     command = input('Введите что вы хотите сделать: ').lower()
-#     if command == 'help':
-#         print('Все комманды:\n'
-#               'help - вывод всех команд\n'
-#               'addphar - добаить в список свою аптеку\n'
-#               'addpill - добавить лекарство в аптеку\n'
-#               'info - вывести информацию аптеки\n'
-#               'highprice - вывести самое дорогое лекарство \n'
-#               'allprice - вывести все цены на лекарства \n')
-#     elif command == 'addphar':
-#         pass
-#     elif command == "addpill":
-#         pass
-#     elif command == "info":
-#         pass
-#     elif command == "highprice":
-#         pass
-#     elif command == "allprice":
-#         pass
-
-
-"""Создание объектов класса в list()"""
-"""Для редактирования попробовать сделать поиск названия аптеки и получить его номер в списке
-в следствие чего использовать его для добавления измененного объекта и удаления старого"""
-
+pharmacy_list = list()
 while True:
-    all_phar = []
-    command = input('Введите команду: ')
-    if command == "create":
-        name = input("Введите название аптеки: ")
+    com = input('Введите команду: ')
+
+    if com == 'create':
+        name = input('Введите название добавляемой аптеки: ')
         phar = Pharmacy(name)
-        print(phar._name)
-        all_phar.append(phar._name)
-    elif command == "exit":
-        print(all_phar)
+        pharmacy_list.append(phar)
+
+    elif com == 'add pill':
+        i = 1
+        print('В какую аптеку вы хотите добавить лекарство (выберите номер)')
+        for phar in pharmacy_list:
+            print(f'{i}) {phar.data["name"]}')
+            i += 1
+        num = int(input('Я выбираю - ')) - 1
+        print('Отлично! А теперь введите название препарата и его цену')
+        name = input('Название: ')
+        cost = input('Стоимость: ')
+        pharmacy_list[num].add_pill(name, cost)
+
+    elif com == 'full info':
+        i = 1
+        print('Выберите аптеку, чью полную информацию вы хотите посмотреть')
+        for phar in pharmacy_list:
+            print(f'{i}) {phar.data["name"]}')
+            i += 1
+        num = int(input('Я выбираю - ')) - 1
+        pharmacy_list[num].full_info()
+
+    elif com == 'total cost':
+        i = 1
+        print('Выберите аптеку, сумму стоимости лекарств которой вы хотите получить')
+        for phar in pharmacy_list:
+            print(f'{i}) {phar.data["name"]}')
+            i += 1
+        num = int(input('Я выбираю - ')) - 1
+        pharmacy_list[num].total_cost()
+
+    elif com == 'exit':
         break
